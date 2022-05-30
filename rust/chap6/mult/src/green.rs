f19b9a703e15d070f1c5c062c749bf0955523e55
daiki328 <dk08sel11@i.softbank.jp>
2022-05-30 18:29:28 +0900

    add: chap6

--- rust/chap6/mult/src/green.rs
+++ rust/chap6/mult/src/green.rs
@@ -43,3 +43,234 @@ extern "C" {
 }
 
 type Entry = fn();
+
+const PAGE_SIZE: usize = 4 * 1024;
+
+struct MappedList<T> {
+    map: HashMap<u64, LinkedList<T>>,
+}
+
+impl<T> MappedList<T> {
+    fn new() -> Self {
+        MappedList {
+            map: HashMap::new(),
+        }
+    }
+
+    fn push_back(&mut self, key: u64, val: T) {
+        if let Some(list) = self.map.get_mut(&key) {
+            list.push_back(val);
+        } else {
+            let mut list = LinkedList::new();
+            list.push_back(val);
+            self.map.insert(key, list);
+        }
+    }
+
+    fn pop_front(&mut self, key: u64) -> Option<T> {
+        if let Some(list) = self.map.get_mut(&key) {
+            let val = list.pop_front();
+            if list.len() == 0 {
+                self.map.remove(&key);
+            }
+            val
+        } else {
+            None
+        }
+    }
+
+    fn clear(&mut self) {
+        self.map.clear();
+    }
+}
+
+struct Context {
+    regs: Registers,
+    stack: *mut u8,
+    stack_layout: Layout,
+    entry: Entry,
+    id: u64,
+}
+
+impl Context {
+    fn get_regs_mut(&mut self) -> *mut Registers {
+        &mut self.regs as *mut Registers
+    }
+
+    fn get_regs(&self) -> *const Registers {
+        &self.regs as *const Registers
+    }
+
+    fn new(func: Entry, stack_size: usize, id: u64) -> Self {
+        let layout = Layout::from_size_align(stack_size, PAGE_SIZE).unwrap();
+        let stack = unsafe { alloc(layout) };
+
+        unsafe {
+            mprotect(stack as *mut c_void, PAGE_SIZE, ProtFlags::PROT_NONE).unwrap();
+        }
+
+        let regs = Registers::new(stack as u64 + stack_size as u64);
+
+        Context {
+            regs,
+            stack,
+            stack_layout: layout,
+            entry: func,
+            id,
+        }
+    }
+}
+
+fn get_id() -> u64 {
+    loop {
+        let rnd = rand::random::<u64>();
+        unsafe {
+            if !(*ID).contains(&rnd) {
+                (*ID).insert(rnd);
+                return rnd;
+            };
+        }
+    }
+}
+
+pub fn spawn(func: Entry, stack_size: usize) -> u64 {
+    unsafe {
+        let id = get_id();
+        CONTEXTS.push_back(Box::new(Context::new(func, stack_size, id)));
+        schedule();
+        id
+    }
+}
+
+pub fn schedule() {
+    unsafe {
+        if CONTEXTS.len() == 1 {
+            return;
+        }
+
+        let mut ctx = CONTEXTS.pop_front().unwrap();
+        let regs = ctx.get_regs_mut();
+        CONTEXTS.push_back(ctx);
+
+        if set_context(regs) == 0 {
+            let next = CONTEXTS.pop_front().unwrap();
+            switch_context((**next).get_regs());
+        }
+
+        rm_unused_stacks();
+    }
+}
+
+extern "C" fn entry_point() {
+    unsafe {
+        let ctx = CONTEXTS.front().unwrap();
+        ((**ctx).entry)();
+
+        let ctx = CONTEXTS.pop_front().unwrap();
+
+        (*ID).remove(&ctx.id);
+
+        UNUSED_STACK = ((*ctx).stack, (*ctx).stack_layout);
+
+        match CONTEXTS.front() {
+            Some(c) => {
+                switch_context((**c).get_regs());
+            }
+            None => {
+                if let Some(c) = &CTX_MAIN {
+                    switch_context(&**c as *const Registers);
+                }
+            }
+        };
+    }
+    panic!("entry_point");
+}
+
+pub fn spawn_from_main(func: Entry, stack_size: usize) -> {
+    unsafe {
+        if let Some(_) = &CTX_MAIN {
+            panic!("spawn_from_main is called twice");
+        }
+
+        CTX_MAIN = Some(Box::new(Registers::new(0)));
+        if let Some(ctx) = &mut CTX_MAIN {
+            let mut msgs = MappedList::new();
+            MESSAGES = &mut msgs as *mut MappedList<u64>;
+
+            let mut waiting = HashMap::new();
+            WAITING = &mut waiting as *mut HashMap<u64, Box<Context>>;
+
+            let mut ids = HashSet::new();
+            ID = &mut ids as *mut HashSet<u64>;
+
+            if set_context(&mut **ctx as *mut Registers) == 0 {
+                CONTEXTS.push_back(Box::new(Context::new(func, stack_size, get_id())));
+                let first = CONTEXTS.front().unwrap();
+                switch_context(first.get_regs());
+            }
+
+            rm_unused_stacks();
+
+            CTX_MAIN = None;
+            CONTEXTS.clear();
+            MESSAGES = ptr::null_mut();
+            WAITING = ptr::null_mut();
+            ID = ptr::null_mut();
+
+            msgs.clear();
+            waiting.clear();
+            ids.clear();
+        }
+    }
+}
+
+unsafe fn rm_unused_stack() {
+    if UNUSED_STACK.0 != ptr::null_mut() {
+        mprotect(
+            UNUSED_STACK.0 as *mut c_void,
+            PAGE_SIZE,
+            ProtFlags::PROT_READ | ProtFlags::PROT_WRITE,
+        )
+            .unwrap();
+        dealloc(UNUSED_STACK.0, UNUSED_STACK.1);
+        UNUSED_STACK = (ptr::null_mut(), Layout::new::<u8>());
+    }
+}
+
+pub fn send(key: u64, msg: u64) {
+    unsafe {
+        (*MESSAGES).push_back(msg);
+
+        if let Some(ctx) = (*WAITING).remove(&key) {
+            CONTEXTS.push_back(ctx);
+        }
+    }
+    schedule();
+}
+
+pub fn recv() -> Option<u64> {
+    unsafe {
+        let key = CONTEXTS.front().unwrap().id;
+
+        if let Some(msg) = (*MESSAGES).pop_front(key) {
+            return Some(msg);
+        }
+
+        if CONTEXTS.len() == 1 {
+            panic!("deadlock");
+        }
+
+        let mut ctx = CONTEXTS.pop_front().unwrap();
+        let regs = ctx.get_regs_mut();
+        (*WAITING).insert(ctx.id, ctx);
+
+        if set_context(regs) == 0 {
+            let next = CONTEXTS.front().unwrap();
+            switch_context((**next).get_regs());
+        }
+
+        rm_unused_stack();
+
+        (*MESSAGES).pop_front(key)
+    }
+}
