0a6fa4131549bc2756014301431d9bcbf7d55fd3
daiki328 <dk08sel11@i.softbank.jp>
2022-06-01 12:52:17 +0900

    fix: chap7

--- rust/chap7/lockfree/src/main.rs
+++ rust/chap7/lockfree/src/main.rs
@@ -4,8 +4,8 @@ use std::sync::Arc;
 
 mod stack;
 
-const NUM_LOOP: usize = 1000000;
-const NUM_THREADS: usize = 4;
+const NUM_LOOP: usize = 1000000; // ループ回数
+const NUM_THREADS: usize = 4;    // スレッド数
 
 use stack::Stack;
 
@@ -17,6 +17,7 @@ fn main() {
         let stack0 = stack.clone();
         let t = std::thread::spawn(move || {
             if i & 1 == 0 {
+                // 偶数スレッドはpush
                 for j in 0..NUM_LOOP {
                     let k = i * NUM_LOOP + j;
                     stack0.get_mut().push(k);
@@ -24,8 +25,10 @@ fn main() {
                 }
                 println!("finished push: #{}", i);
             } else {
+                // 奇数スレッドはpop
                 for _ in 0..NUM_LOOP {
                     loop {
+                        // pop、Noneの場合やり直し
                         if let Some(k) = stack0.get_mut().pop() {
                             println!("pop: {}", k);
                             break;
@@ -42,5 +45,5 @@ fn main() {
         t.join().unwrap();
     }
 
-    assert!(stack.get().pop() == None);
+    assert!(stack.get_mut().pop() == None);
 }
--- rust/chap7/lockfree/src/stack.rs
+++ rust/chap7/lockfree/src/stack.rs
@@ -1,11 +1,14 @@
 use std::ptr::null_mut;
+use std::arch::asm;
 
+// スタックのノード。リスト構造で管理 <1>
 #[repr(C)]
 struct Node<T> {
     next: *mut Node<T>,
     data: T,
 }
 
+// スタックの先頭 <2>
 #[repr(C)]
 pub struct StackHead<T> {
     head: *mut Node<T>,
@@ -16,59 +19,73 @@ impl<T> StackHead<T> {
         StackHead { head: null_mut() }
     }
 
-    pub fn push(&mut self, v: T) {
+    pub fn push(&mut self, v: T) { // <3>
+        // 追加するノードを作成
         let node = Box::new(Node {
             next: null_mut(),
             data: v,
         });
 
+        // Box型の値からポインタを取り出す
         let ptr = Box::into_raw(node) as *mut u8 as usize;
 
+        // ポインタのポインタを取得
+        // headの格納されているメモリをLL/SC
         let head = &mut self.head as *mut *mut Node<T> as *mut u8 as usize;
 
+        // LL/SCを用いたpush <4>
         unsafe {
-            asm!("1:\
-                ldxr {next}, [{head}]\
-                str {next}, [{ptr}]\
-                stlxr w10, {ptr}, [{head}]\
-                cbnz w10, 1b",
-                next = out(reg) _,
-                ptr = in(reg) ptr,
-                head = in(reg) head,
-                out("w10") _,
-            )
+            asm!("1:
+                  ldxr {next}, [{head}] // next = *head
+                  str {next}, [{ptr}]   // *ptr = next
+                  stlxr w10, {ptr}, [{head}] // *head = ptr
+                  // if tmp != 0 then goto 1
+                  cbnz w10, 1b",
+            next = out(reg) _,
+            ptr = in(reg) ptr,
+            head = in(reg) head,
+            out("w10") _)
         };
     }
 
-    pub fn pop(&mut self) -> Option<T> {
+    pub fn pop(&mut self) -> Option<T> { // <5>
         unsafe {
+            // ポインタのポインタを取得
+            // headの格納されているメモリをLL/SC
             let head = &mut self.head as *mut *mut Node<T> as *mut u8 as usize;
 
+            // popしたノードへのアドレスを格納
             let mut result: usize;
 
-            asm!("1:\
-                ldaxr {result}, [{head}]\
-                cbnz {result}, 2f\
+            // LL/SCを用いたpop <6>
+            asm!("1:
+                  ldaxr {result}, [{head}] // result = *head
+                  // if result != NULL then goto 2
+                  cbnz {result}, 2f
 
-                clrex\
-                b 3f\
+                  // if NULL
+                  clrex // clear exclusive
+                  b 3f  // goto 3
 
-                2:\
-                ldr {next}, [{result}]\
-                stxr w10, {next}, [{head}]\
-                cbnz w10, 1b\
+                  // if not NULL
+                  2:
+                  ldr {next}, [{result}]     // next = *result
+                  stxr w10, {next}, [{head}] // *head = next
+                  // if tmp != 0 then goto 1
+                  cbnz w10, 1b
 
-                3:",
-                next = out(reg) _,
-                result = out(reg) result,
-                head = in(reg) head,
-                out("w10") _,
-            );
+                  3:",
+            next = out(reg) _,
+            result = out(reg) result,
+            head = in(reg) head,
+            out("w10") _);
 
             if result == 0 {
                 None
             } else {
-                let head = Box::from_raw(result as *mut u8 as *mut Node<T>);
+                // ポインタをBoxに戻して、中の値をリターン
+                let ptr = result as *mut u8 as *mut Node<T>;
+                let head = Box::from_raw(ptr);
                 Some((*head).data)
             }
         }
@@ -77,9 +94,10 @@ impl<T> StackHead<T> {
 
 impl<T> Drop for StackHead<T> {
     fn drop(&mut self) {
+        // データ削除
         let mut node = self.head;
-
         while node != null_mut() {
+            // ポインタをBoxに戻す操作を繰り返す
             let n = unsafe { Box::from_raw(node) };
             node = n.next;
         }
@@ -88,6 +106,7 @@ impl<T> Drop for StackHead<T> {
 
 use std::cell::UnsafeCell;
 
+// StackHeadをUnsafeCellで保持するのみ
 pub struct Stack<T> {
     data: UnsafeCell<StackHead<T>>,
 }
@@ -104,5 +123,6 @@ impl<T> Stack<T> {
     }
 }
 
+// スレッド間のデータ共有と、チャネルを使った送受信が可能と設定
 unsafe impl<T> Sync for Stack<T> {}
 unsafe impl<T> Send for Stack<T> {}
